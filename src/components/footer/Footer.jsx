a9b78c717e5ca085e1a57fbdb1ad14e0ab393381
Rahul Jaiswal <rahul.jaiswal1997@gmail.com>
2023-02-16 23:44:14 +0530

    footer updated

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./footer.css"
+function Footer() {
+  return (
+    <div>
+      <div className="fLists">
+        <div className="fList">
+            <li className="fListItem">Countries</li>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
