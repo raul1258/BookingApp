84ca17407b34ff967d4a725a5fb7f0e8a337ddb0
Rahul Jaiswal <rahul.jaiswal1997@gmail.com>
2023-02-21 21:45:43 +0530

    footer updated

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom';
 import Header from '../../components/header/Header'
 import Navbar from '../../components/navbar/Navbar'
 import "./list.css";
 
 function List() {
+
+  const location =useLocation()
+  console.log(location)
   return (
     <div>
       <Navbar/>
