310db248b4870042c0c3a0833470ddec2ad143d4
Rahul Jaiswal <rahul.jaiswal1997@gmail.com>
2023-02-25 21:28:23 +0530

    hotel page updated

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -21,7 +21,7 @@ function List() {
           <div className="listSearch">
             <h1 className="lsTitle">Search</h1>
             <div className="lsItem">
-              <label htmlFor="" placeholder={destination}>Destination</label>
+              <label htmlFor="">Destination</label>
               <input type="text" />
             </div>
             <div className="lsItem">
